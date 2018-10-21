e2cf04cef6d1a8040d51e5ba0f8a6d02ae0e5f13
mattpaul <mattpaul@gmail.com>
2018-10-21 15:47:08 -0700

    add event interface, mock data

---
+++ src/data/events.ts
@@ -0,0 +1,38 @@
+export default [
+  {
+    'artist': 'Erika Badu',
+    'image': 'assets/mocks/events/badu-live.png',
+    'images': [
+      'assets/mocks/events/badu-live.png',
+      'assets/mocks/events/badu-live.png',
+      'assets/mocks/events/badu-live.png',
+    ],
+    'venue': 'Bill Graham',
+    'date': '2018-10-01',
+    'setlist': "song 1\nsong 2\nsong 3",
+  },
+  {
+    'artist': 'Nine Inch Nails',
+    'image': 'assets/mocks/events/nin-live.png',
+    'images': [
+      'assets/mocks/events/nin-live.png',
+      'assets/mocks/events/nin-live.png',
+      'assets/mocks/events/nin-live.png',
+    ],
+    'venue': 'The Fillmore',
+    'date': '2017-12-31',
+    'setlist': "song 1\nsong 2\nsong 3",
+  },
+  {
+    'artist': 'Run DMC',
+    'image': 'assets/mocks/events/rundmc-live.png',
+    'images': [
+      'assets/mocks/events/rundmc-live.png',
+      'assets/mocks/events/rundmc-live.png',
+      'assets/mocks/events/rundmc-live.png',
+    ],
+    'venue': 'The Midway',
+    'date': '1996-04-20',
+    'setlist': "song 1\nsong 2\nsong 3",
+  }
+]
---
+++ src/interfaces/event.interface.ts
@@ -0,0 +1,8 @@
+export interface Event {
+  'artist': string;
+  'date': string;
+  'image': string;
+  'images': string[];
+  'setlist': string;
+  'venue': string;
+}
