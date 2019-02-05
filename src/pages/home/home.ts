85daff49398f126b93323134ed37cc8daee36a2a
mattpaul <mattpaul@gmail.com>
2019-02-04 21:32:16 -0800

    update home feed to use events service

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,26 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { EventDetailsImagesPage } from '../event-details-images/event-details-images';
-import { Event } from "../../interfaces/event.interface";
-import events from '../../data/events'
+import { Event, EventResponse } from '../../interfaces/event.interface';
+import { EventService } from '../../services/event.service';
+import events from '../../data/events';
 
 @IonicPage()
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
+  providers: [EventService]
 })
 export class HomePage implements OnInit {
   eventDetailsPage = EventDetailsImagesPage;
   events: Event[];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, 
+              public navParams: NavParams, 
+              private eventService: EventService) {
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad HomePage');
+    // console.log('ionViewDidLoad HomePage');
   }
 
   ngOnInit() {
-    this.events = events;
+    //this.events = events;
+    this.eventService.getAttendedEvents()
+      .subscribe((json: EventResponse) => {
+        console.log(json);
+        this.events = json.events;
+      },
+      error => console.log(error));
   }
 }
