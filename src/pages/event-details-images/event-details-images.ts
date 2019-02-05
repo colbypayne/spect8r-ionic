99f98d5ee45ac86d57ea80da282664ce787d4b9b
mattpaul <mattpaul@gmail.com>
2019-02-04 21:32:57 -0800

    update EventDetailsImagesPage to consume EventResponse

--- src/pages/event-details-images/event-details-images.ts
+++ src/pages/event-details-images/event-details-images.ts
@@ -13,18 +13,34 @@ export class EventDetailsImagesPage {
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     this.event = navParams.data;
-    this.segment = 'images';
+    this.segment = 'settings';
   }
 
-  getDetails(key) {
-    return this.event[key];
+  get headlinerLabel(): string {
+    return this.event.headliningArtists.length > 1 ? 'Headliners' : 'Artist';
+  }
+
+  get supportingLabel(): string {
+    return this.event.supportingArtists.length > 0 ? 'Support' : '';
+  }
+
+  get venueName(): string {
+    return this.event.venue !== undefined ? this.event.venue.venueName : '';
+  }
+
+  get date(): string {
+    return this.event.eventDate !== undefined ? this.event.eventDate : '';
+  }
+
+  get setlistUrl(): string {
+    return this.event.setlist !== undefined ? this.event.setlist.url : '';
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad EventDetailsImagesPage');
+    // console.log('ionViewDidLoad EventDetailsImagesPage');
   }
 
   segmentChanged() {
-    console.log('segmentChanged: ' + this.segment);
+    // console.log('segmentChanged: ' + this.segment);
   }
 }
